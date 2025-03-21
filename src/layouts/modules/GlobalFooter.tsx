e5aa79a69d1ffc4549567abbebf031dff6ad27af
MengChen <tatsukimengchen@163.com>
2025-03-21 10:44:37 +0800

    chore: init

---
+++ src/layouts/modules/GlobalFooter.tsx
@@ -0,0 +1,16 @@
+import DarkModeContainer from '@/components/DarkModeContainer';
+
+const GlobalFooter = () => {
+  return (
+    <DarkModeContainer className="h-full flex-center">
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Copyright © 2025 MengChen
+      </a>
+    </DarkModeContainer>
+  );
+};
+
+export default GlobalFooter;
